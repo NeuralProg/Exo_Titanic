b3072893ad93227e4d81afd7c2962abeff3fb7c0
NeuralPrg <chabaneyann@gmail.com>
2023-05-12 08:29:31 +0200

    initial commit

---
+++ K+procheQ1.py
@@ -0,0 +1,18 @@
+import pandas
+import matplotlib.pyplot as plt
+
+# Traitement csv
+iris = pandas.read_csv ("iris.csv")
+x = iris.loc [:,"petal_length"]
+y = iris.loc [:,"petal_width"]
+lab = iris.loc [:,"species"]
+
+# Graphique
+plt.axis ('equal')
+plt.scatter (x [lab == 0], y [lab == 0], color ='g', label = 'setosa')
+plt.scatter (x [lab == 1], y [lab == 1], color ='r', label = 'versicolor')
+plt.scatter (x [lab == 2], y [lab == 2], color ='b', label = 'virginica')
+plt.legend ()
+plt.show ()
+
+print()
